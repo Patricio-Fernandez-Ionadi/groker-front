00a1e6ba61bcfc5731d70d67b5eb2c0e8cfd431c
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-01-28 11:19:43 -0300

    ft datos de riego especificos

--- src/components/EditPlant.jsx
+++ src/components/EditPlant.jsx
@@ -3,6 +3,8 @@ import { PlantContext } from '../context/PlantContext'
 import { ProductContext } from '../context/ProductContext'
 import useEditPlantForm from '../hooks/useEditPlantForm'
 import { translateField } from '../utils/translations'
+import WateringForm from './WateringForm'
+import PlantDetailsForm from './PlantDetailsForm'
 
 /**
  * Componente para editar los detalles de una planta específica.
@@ -27,43 +29,13 @@ const EditPlant = () => {
 		ec: '',
 		temperature: '',
 		humidity: '',
+		lastWatered: new Date().toISOString().split('T')[0], // Fecha actual por defecto
 	})
 
-	const handleWateringChange = (e, index) => {
-		const { name, value } = e.target
-		if (name === 'product' || name === 'productAmount') {
-			const newProductsUsed = [...wateringData.productsUsed]
-			newProductsUsed[index][name] = value
-			setWateringData({
-				...wateringData,
-				productsUsed: newProductsUsed,
-			})
-		} else {
-			setWateringData({
-				...wateringData,
-				[name]: value,
-			})
-		}
-	}
+	const [isWatered, setIsWatered] = useState(false)
 
-	const addProductField = () => {
-		setWateringData({
-			...wateringData,
-			productsUsed: [
-				...wateringData.productsUsed,
-				{ product: '', productAmount: '' },
-			],
-		})
-	}
-
-	const removeProductField = (index) => {
-		const newProductsUsed = wateringData.productsUsed.filter(
-			(_, i) => i !== index
-		)
-		setWateringData({
-			...wateringData,
-			productsUsed: newProductsUsed,
-		})
+	const handleCheckboxChange = () => {
+		setIsWatered(!isWatered)
 	}
 
 	const handleSubmit = (e) => {
@@ -72,20 +44,24 @@ const EditPlant = () => {
 			const changeDescription = `Planta actualizada: ${Object.keys(plantData)
 				.map((key) => `${translateField(key)}: ${plantData[key]}`)
 				.join(', ')}`
+
 			updatePlant(plantData, changeDescription, wateringData, products)
+
 			if (note.trim()) {
 				addNote(selectedPlant.id, note.trim())
 				setNote('')
 			}
+
 			// Asegurarse de que ambos productos se actualicen correctamente en el stock
 			wateringData.productsUsed.forEach((productUsed) => {
 				if (productUsed.product && productUsed.productAmount) {
 					updateProductStock(
 						productUsed.product,
-						productUsed.productAmount // Asegurarse de restar la cantidad correcta
+						-parseFloat(productUsed.productAmount) // Pasar la cantidad como negativa
 					)
 				}
 			})
+
 			// Limpiar los campos del formulario
 			setWateringData({
 				amount: '',
@@ -94,6 +70,7 @@ const EditPlant = () => {
 				ec: '',
 				temperature: '',
 				humidity: '',
+				lastWatered: new Date().toISOString().split('T')[0], // Fecha actual por defecto
 			})
 		}
 	}
@@ -102,199 +79,47 @@ const EditPlant = () => {
 
 	return (
 		<form onSubmit={handleSubmit}>
-			{showAdvancedFields && (
-				<>
-					<label>
-						Fecha de ingreso:
-						<input
-							type="date"
-							name="entryDate"
-							value={plantData.entryDate || ''}
-							onChange={handleChange}
-							placeholder="Fecha de ingreso"
-						/>
-					</label>
-					<label>
-						Nombre de la planta:
-						<input
-							type="text"
-							name="name"
-							value={plantData.name || ''}
-							onChange={handleChange}
-							placeholder="Nombre de la planta"
-						/>
-					</label>
-					<label>
-						Genética:
-						<input
-							type="text"
-							name="genetic"
-							value={plantData.genetic || ''}
-							onChange={handleChange}
-							placeholder="Genética"
-						/>
-					</label>
-				</>
-			)}
-			<label>
-				Etapa:
-				<select
-					name="stage"
-					value={plantData.stage || 'Vegetativo'}
-					onChange={handleChange}
-				>
-					<option value="Vegetativo">Vegetativo</option>
-					<option value="Floracion">Floración</option>
-				</select>
-			</label>
-			<label>
-				Cambio estimado:
-				<input
-					type="date"
-					name="estimatedChange"
-					value={plantData.estimatedChange || ''}
-					onChange={handleChange}
-					placeholder="Cambio estimado"
-				/>
-			</label>
-			<label>
-				Último riego:
-				<input
-					type="date"
-					name="lastWatered"
-					value={plantData.lastWatered || ''}
-					onChange={handleChange}
-					placeholder="Último riego"
-				/>
-			</label>
-			<label>
-				Tamaño de la maceta:
-				<input
-					type="text"
-					name="potSize"
-					value={plantData.potSize || ''}
-					onChange={handleChange}
-					placeholder="Tamaño de la maceta"
-				/>
-			</label>
-			<label>
-				<input
-					type="checkbox"
-					name="isFinalPot"
-					checked={plantData.isFinalPot || false}
-					onChange={handleChange}
-				/>
-				Maceta final
-			</label>
-			<label>
-				<input
-					type="checkbox"
-					name="underObservation"
-					checked={plantData.underObservation || false}
-					onChange={handleChange}
-				/>
-				Bajo observación
-			</label>
-			<label>
-				Notas:
-				<textarea
-					name="note"
-					value={note}
-					onChange={handleNoteChange}
-					placeholder="Añadir una nota"
-				/>
-			</label>
-			<label>
-				Cantidad de agua (ml):
-				<input
-					type="number"
-					name="amount"
-					value={wateringData.amount}
-					onChange={handleWateringChange}
-					placeholder="Cantidad de agua (ml)"
-				/>
-			</label>
-			{wateringData.productsUsed.map((productUsed, index) => (
-				<div key={index}>
-					<label>
-						Producto:
-						<select
-							name="product"
-							value={productUsed.product}
-							onChange={(e) => handleWateringChange(e, index)}
-						>
-							<option value="">Seleccionar producto</option>
-							{products.map((product) => (
-								<option key={product.id} value={product.id}>
-									{product.name}
-								</option>
-							))}
-						</select>
-					</label>
-					<label>
-						Cantidad de producto (ml):
-						<input
-							type="number"
-							name="productAmount"
-							value={productUsed.productAmount}
-							onChange={(e) => handleWateringChange(e, index)}
-							placeholder="Cantidad de producto (ml)"
-						/>
-					</label>
-					<button type="button" onClick={() => removeProductField(index)}>
-						Eliminar Producto
-					</button>
-				</div>
-			))}
-			<button type="button" onClick={addProductField}>
-				Añadir Producto
-			</button>
-			<label>
-				pH del agua:
-				<input
-					type="number"
-					name="ph"
-					value={wateringData.ph}
-					onChange={handleWateringChange}
-					placeholder="pH del agua"
-				/>
-			</label>
-			<label>
-				EC del agua:
-				<input
-					type="number"
-					name="ec"
-					value={wateringData.ec}
-					onChange={handleWateringChange}
-					placeholder="EC del agua"
-				/>
-			</label>
-			<label>
-				Temperatura (°C):
-				<input
-					type="number"
-					name="temperature"
-					value={wateringData.temperature}
-					onChange={handleWateringChange}
-					placeholder="Temperatura (°C)"
-				/>
-			</label>
-			<label>
-				Humedad (%):
-				<input
-					type="number"
-					name="humidity"
-					value={wateringData.humidity}
-					onChange={handleWateringChange}
-					placeholder="Humedad (%)"
-				/>
-			</label>
-			<button type="button" onClick={toggleAdvancedFields}>
-				{showAdvancedFields
-					? 'Ocultar Campos Avanzados'
-					: 'Mostrar Campos Avanzados'}
-			</button>
-			<button type="submit">Guardar Cambios</button>
+			<PlantDetailsForm
+				plantData={plantData}
+				handleChange={handleChange}
+				showAdvancedFields={showAdvancedFields}
+			/>
+			<div className="form-group">
+				<label>
+					<input
+						type="checkbox"
+						checked={isWatered}
+						onChange={handleCheckboxChange}
+					/>
+					¿Hubo riego?
+				</label>
+				{isWatered && (
+					<WateringForm
+						wateringData={wateringData}
+						setWateringData={setWateringData}
+						products={products}
+					/>
+				)}
+			</div>
+			<div className="form-group">
+				<label>
+					Notas:
+					<textarea
+						name="note"
+						value={note}
+						onChange={handleNoteChange}
+						placeholder="Añadir una nota"
+					/>
+				</label>
+			</div>
+			<div className="form-group">
+				<button type="button" onClick={toggleAdvancedFields}>
+					{showAdvancedFields
+						? 'Ocultar Campos Avanzados'
+						: 'Mostrar Campos Avanzados'}
+				</button>
+				<button type="submit">Guardar Cambios</button>
+			</div>
 		</form>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import App from './app/App'
 import { PlantProvider } from './context/PlantContext'
+import App from './app/App'
+
+// import './styles.css'
 
 createRoot(document.getElementById('root')).render(
 	<PlantProvider>
