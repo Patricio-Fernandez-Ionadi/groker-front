8a6dc1b66e515be51168fd1a77f3247faa311513
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-01-29 09:52:29 -0300

    add theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { PlantProvider } from './context/PlantContext'
 import App from './app/App'
 
-// import './styles.css'
+import './theme/index.css'
 
 createRoot(document.getElementById('root')).render(
 	<PlantProvider>
