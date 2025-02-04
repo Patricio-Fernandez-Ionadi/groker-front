548946050576d88f5d76bce5f130fda01eac4c52
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-03 21:57:54 -0300

    del eliminacion metodo no usado api_updateHistory

--- src/api/plants.js
+++ src/api/plants.js
@@ -53,9 +53,11 @@ export const api_deletePlant = async (plantId) => {
 	}
 }
 
-// ----- TODO -----
-
-// Ejemplo de cómo editar una planta
+/**
+ * Edita los datos de una planta existente enviando una solicitud PUT a la API.
+ * @param {Object} plantData - Los nuevos datos de la planta, incluyendo su ID.
+ * @returns {Promise<Object>} - Los datos de la planta editada.
+ */
 export const api_editPlant = async (plantData) => {
 	try {
 		const response = await fetch(`/api/plants/${plantData._id}`, {
@@ -70,23 +72,5 @@ export const api_editPlant = async (plantData) => {
 		return data
 	} catch (error) {
 		console.error('Error al editar planta:', error)
-	}
-}
-
-// Ejemplo de cómo agregar un registro al historial de una planta
-export const api_addHistoryEntry = async (plantId, historyData) => {
-	try {
-		const response = await fetch(`/api/plants/${plantId}/history`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(historyData),
-		})
-		const data = await response.json()
-		// console.log('Entrada de historial agregada:', data)
-		return data
-	} catch (error) {
-		console.error('Error al agregar entrada de historial:', error)
 	}
 }
