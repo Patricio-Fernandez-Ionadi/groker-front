c30f2152ed599998fa805a31e02d7cecea8b5866
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-15 02:35:08 -0300

    del boton de nuevo ingreso

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,21 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
-import { FormContext, usePath } from '../'
+import { usePath } from '../'
 
 export function Header() {
-	const { toggleAddPlantForm, isAddPlantFormOpen } = useContext(FormContext)
-
 	const currentPath = usePath()
 
 	return (
 		<header className="header">
 			<h1>Gestión de Inventario de Cultivos</h1>
 			<div className="header-buttons">
-				<button onClick={toggleAddPlantForm}>
-					{isAddPlantFormOpen ? 'Cerrar' : 'Nuevo Ingreso'}
-				</button>
-
 				<button>
 					{currentPath !== '/products' && (
 						<Link to="/products">Inventario de Productos</Link>
