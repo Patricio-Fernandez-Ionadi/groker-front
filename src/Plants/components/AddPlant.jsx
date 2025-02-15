5a01a98520bab8647c69d6813f47fbe80bdcf7d1
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-15 02:36:07 -0300

    uso de hooks y boton de cancel

--- src/Plants/components/AddPlant.jsx
+++ src/Plants/components/AddPlant.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
 
-import { AddGeneticButton } from '../../Genetics'
 import { FormContext, ToggleSwitch } from '../../app'
+import { AddGeneticButton, useGenetics } from '../../Genetics'
 
-import { useDispatch, useSelector } from 'react-redux'
-import { addPlant, validatePlantData } from '../index'
+import { validatePlantData, usePlantsActions } from '../'
 
 const defaultPlantData = {
 	entryDate: '',
@@ -15,13 +15,14 @@ const defaultPlantData = {
 }
 
 export const AddPlant = () => {
+	const location = useLocation()
+	const navigate = useNavigate()
+	const { closeAddPlantForm } = useContext(FormContext)
+	const { genetics } = useGenetics()
+	const { addNewPlant } = usePlantsActions()
+
 	const [newPlantData, setPlantData] = useState(defaultPlantData)
 	const [errors, setErrors] = useState({})
-
-	const { closeAddPlantForm } = useContext(FormContext)
-
-	const dispatch = useDispatch()
-	const { genetics } = useSelector((state) => state.geneticsStore)
 
 	/**
 	 * Maneja los cambios en los campos del formulario.
@@ -51,91 +52,108 @@ export const AddPlant = () => {
 			)
 
 			try {
-				dispatch(addPlant(newPlantData))
+				addNewPlant(newPlantData)
 				setPlantData(defaultPlantData)
 				setErrors({})
-				closeAddPlantForm()
+				navigate('/')
 			} catch (error) {
 				console.error('Error al agregar planta AddPlant.jsx:', error)
 			}
 		}
 	}
 
+	const handleCancelForm = () => {
+		if (location.state && location.state.from) {
+			navigate(location.state.from)
+		} else if (location.pathname === '/') {
+			closeAddPlantForm()
+		} else {
+			navigate('/')
+		}
+	}
+
 	return (
-		<div className="add-plant-form-container">
-			<div className="add-plant-form-name">
+		<>
+			<div className="add-plant-form-container">
+				<button className="cancel-button" onClick={handleCancelForm}>
+					Cerrar
+				</button>
+				<div className="add-plant-form-name">
+					<input
+						type="text"
+						name="name"
+						value={newPlantData.name}
+						onChange={handleChange}
+						placeholder="Nombre de la planta"
+					/>
+					{errors.name && <span className="error">{errors.name}</span>}
+				</div>
+
+				<div className="add-plant-form-genetic">
+					<select
+						name="genetic"
+						defaultValue={newPlantData.genetic.name}
+						onChange={handleChange}
+					>
+						<option>Seleccionar genética</option>
+						{genetics.map((gen) => (
+							<option key={gen._id} value={gen.name}>
+								{gen.name}
+							</option>
+						))}
+					</select>
+					<AddGeneticButton />
+				</div>
+
+				<label className="add-plant-form-date">
+					<p>Fecha de Ingreso</p>
+					<input
+						type="date"
+						name="entryDate"
+						value={newPlantData.entryDate}
+						onChange={handleChange}
+					/>
+					{errors.entryDate && (
+						<span className="error">{errors.entryDate}</span>
+					)}
+				</label>
+
+				<label className="add-plant-form-stage">
+					<p>Etapa:</p>
+					<select name="stage" onChange={handleChange}>
+						<option value="vegetative">Vegetativo</option>
+						<option value="flowering">Floración</option>
+						<option value="germination">Germinacion</option>
+					</select>
+				</label>
+
 				<input
-					type="text"
-					name="name"
-					value={newPlantData.name}
+					className="add-plant-form-potSize"
+					type="number"
+					name="potSize"
+					value={newPlantData.potSize}
 					onChange={handleChange}
-					placeholder="Nombre de la planta"
+					placeholder="Tamaño de la maceta"
 				/>
-				{errors.name && <span className="error">{errors.name}</span>}
+
+				<label className="add-plant-form-potCheckbox">
+					<p>Maceta final</p>
+					<ToggleSwitch
+						name={'isFinalPot'}
+						onEvent={(e) =>
+							setPlantData({
+								...newPlantData,
+								flags: { ...newPlantData.flags, isFinalPot: e.target.checked },
+							})
+						}
+						switcher={newPlantData.flags.isFinalPot}
+					/>
+				</label>
+
+				<button className="add-plant-form-button" onClick={handleSubmit}>
+					Añadir Planta
+				</button>
 			</div>
-
-			<div className="add-plant-form-genetic">
-				<select
-					name="genetic"
-					defaultValue={newPlantData.genetic.name}
-					onChange={handleChange}
-				>
-					<option>Seleccionar genética</option>
-					{genetics.map((gen) => (
-						<option key={gen._id} value={gen.name}>
-							{gen.name}
-						</option>
-					))}
-				</select>
-				<AddGeneticButton />
-			</div>
-
-			<label className="add-plant-form-date">
-				<p>Fecha de Ingreso</p>
-				<input
-					type="date"
-					name="entryDate"
-					value={newPlantData.entryDate}
-					onChange={handleChange}
-				/>
-				{errors.entryDate && <span className="error">{errors.entryDate}</span>}
-			</label>
-
-			<label className="add-plant-form-stage">
-				<p>Etapa:</p>
-				<select name="stage" onChange={handleChange}>
-					<option value="vegetative">Vegetativo</option>
-					<option value="flowering">Floración</option>
-					<option value="germination">Germinacion</option>
-				</select>
-			</label>
-
-			<input
-				className="add-plant-form-potSize"
-				type="number"
-				name="potSize"
-				value={newPlantData.potSize}
-				onChange={handleChange}
-				placeholder="Tamaño de la maceta"
-			/>
-
-			<label className="add-plant-form-potCheckbox">
-				<p>Maceta final</p>
-				<ToggleSwitch
-					name={'isFinalPot'}
-					onEvent={(e) =>
-						setPlantData({
-							...newPlantData,
-							flags: { ...newPlantData.flags, isFinalPot: e.target.checked },
-						})
-					}
-					switcher={newPlantData.flags.isFinalPot}
-				/>
-			</label>
-
-			<button className="add-plant-form-button" onClick={handleSubmit}>
-				Añadir Planta
-			</button>
-		</div>
+		</>
 	)
 }
