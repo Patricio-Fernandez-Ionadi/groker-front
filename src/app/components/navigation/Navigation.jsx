bd94f3be1a7dd941511c0e904ee743cb70712448
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-17 17:01:59 -0300

    nav list on process

--- src/app/components/navigation/Navigation.jsx
+++ src/app/components/navigation/Navigation.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router'
 
+import { useTheme } from '../../context/ThemeContext'
+
 import { BurguerIcon } from './BurguerIcon'
 import { BackButton } from './BackButton'
+
 import { routes } from '../../utils/routes'
+import { ToggleSwitch } from '../ToggleSwitch'
+import { Dark_icon, Light_icon } from '../../../assets/Icons'
 
 export const Navigation = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	const { toggleTheme, isDarkMode, theme } = useTheme()
 
 	const location = useLocation()
 	const prevRoute = location.state?.from
 
-	const isBackAvailable = location.state && location.pathname !== routes.home
+	const isBackAvailable =
+		location.pathname !== routes.home && location.state != null
+
+	const sendFromLocation = {
+		from: location.pathname,
+	}
 
 	return (
 		<div className="navigation-container">
 			<div>
-				<div className="burguer-button">
+				<div className={`burguer-button ${theme}`}>
 					<BurguerIcon toggle={isOpen} onEvent={() => setIsOpen(!isOpen)} />
 				</div>
 
 				<div className="back-button">
-					{isBackAvailable ? <BackButton route={prevRoute} size={30} /> : null}
+					{isBackAvailable ? (
+						<BackButton
+							route={prevRoute}
+							size={30}
+							color={`${theme === 'light' ? '#212121' : '#f5f5f5'}`}
+						/>
+					) : null}
 				</div>
 			</div>
 
@@ -29,11 +46,45 @@ export const Navigation = () => {
 				<h1>Gestión de Inventario de Cultivos</h1>
 			</Link>
 
-			<nav className={`navigation ${isOpen ? 'open' : ''}`}>
+			<nav className={`navigation ${isOpen ? 'open' : ''} ${theme}`}>
+				<div className="theme-switch">
+					<div>
+						{theme === 'dark' ? (
+							<Light_icon size={25} color={'#ff9800'} />
+						) : (
+							<Dark_icon size={25} />
+						)}
+					</div>
+					<ToggleSwitch onEvent={toggleTheme} switcher={isDarkMode} />
+				</div>
 				<ul className="navigation-list">
-					<li>Inicio</li>
-					<li>Inventario</li>
-					<li>Productos</li>
+					<li>
+						<Link
+							to={routes.home}
+							state={sendFromLocation}
+							onClick={(e) => setIsOpen(false)}
+						>
+							Inicio
+						</Link>
+					</li>
+					<li>
+						<Link
+							to={routes.plants}
+							state={sendFromLocation}
+							onClick={(e) => setIsOpen(false)}
+						>
+							Inventario
+						</Link>
+					</li>
+					<li>
+						<Link
+							to={routes.products}
+							state={sendFromLocation}
+							onClick={(e) => setIsOpen(false)}
+						>
+							Productos
+						</Link>
+					</li>
 				</ul>
 			</nav>
 		</div>
