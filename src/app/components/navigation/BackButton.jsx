12c044495f79bf59cd12e2ecf51d7f5a66573b37
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-17 01:50:15 -0300

    fr recives color prop

--- src/app/components/navigation/BackButton.jsx
+++ src/app/components/navigation/BackButton.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router'
 import { Arrow_left } from '../../../assets/Icons'
 
-export function BackButton({ route, size }) {
+export const BackButton = ({ route, size, color }) => {
 	return (
 		<Link to={route}>
-			<Arrow_left size={size} strokeWidth={0.5} />
+			<Arrow_left size={size} strokeWidth={0.5} color={color} />
 		</Link>
 	)
 }
