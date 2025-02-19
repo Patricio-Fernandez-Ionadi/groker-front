d10d13266f5be29c6baabda3c4e389ffaa3c9bc0
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-19 14:44:27 -0300

    refactor: simplify Router component by removing redundant elements

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -9,26 +9,10 @@ import { ProductList } from '../Products'
 export const Router = () => {
 	return (
 		<Routes>
-			<Route
-				index
-				element={
-					<main className="main-content">
-						<Inventory />
-						<Detail />
-					</main>
-				}
-			/>
+			<Route index element={<Inventory />} />
 
 			<Route path={routes.plants.path}>
-				<Route
-					index
-					element={
-						<>
-							<Inventory />
-							<Detail />
-						</>
-					}
-				/>
+				<Route index element={<Inventory />} />
 				<Route path={routes.plantAdd.path} element={<AddPlant />} />
 				<Route path={routes.plantDetail.path} element={<PlantInfo />} />
 			</Route>
