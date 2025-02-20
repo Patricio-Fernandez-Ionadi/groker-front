bccb77f899c1da95a034f32c1092eae14b889244
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-20 14:47:44 -0300

    feat: update VITE_API_URL to VITE_API_URL_DEV for development environment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 dotenv.config()
+console.log(process.env.VITE_API_URL)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: process.env.VITE_API_URL,
+				target: process.env.VITE_API_URL_DEV,
 				changeOrigin: true,
 				secure: false,
 			},
