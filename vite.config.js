465f1fe63572bf25109d6a69ba72991cb9893ef3
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-20 17:01:15 -0300

    fix: remove console log for VITE_API_URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 dotenv.config()
-console.log(process.env.VITE_API_URL)
 
 // https://vitejs.dev/config/
 export default defineConfig({
