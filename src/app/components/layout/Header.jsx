eb9e0618551cf191d25aa889cf21557d5e42d668
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-20 19:55:17 -0300

    fix: remove commented-out code in Header component for cleaner structure

--- src/app/components/layout/Header.jsx
+++ src/app/components/layout/Header.jsx
@@ -6,23 +6,6 @@ export function Header() {
 	return (
 		<header className="header">
 			<Navigation />
-			{/* 
-			{location.state || currentPath !== '/' ? (
-				<div className="back-button">
-					<BackButton />
-				</div>
-			) : (
-				''
-			)}
-			<div className="header-buttons">
-				{currentPath !== '/products' && (
-					<button>
-						<Link to="/products" state={{ from: location.pathname }}>
-							Inventario de Productos
-						</Link>
-					</button>
-				)}
-			</div> */}
 		</header>
 	)
 }
