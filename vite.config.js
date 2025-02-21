4cbb385f50634057138e62f2244991b21d11ff81
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-21 02:08:30 -0300

    feat: add jsconfig.json for improved path resolution and update vite.config.js with module aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -15,6 +16,20 @@ console.log('API URL:', apiUrl)
 export default defineConfig({
 	plugins: [react()],
 	base: './',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'Groker/date': path.resolve(__dirname, './src/app/modules/date/dates.js'),
+			'Groker/components': path.resolve(
+				__dirname,
+				'./src/app/modules/components/index.js'
+			),
+			'Groker/icons': path.resolve(
+				__dirname,
+				'./src/app/modules/icons/Icons.jsx'
+			),
+		},
+	},
 	server: {
 		proxy: {
 			'/api': {
